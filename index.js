76e614f10c2790747b1d7e34fd542a299f28dcda
randomXMode <158544855+randomXMode@users.noreply.github.com>
2024-02-22 10:24:34 +0000

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-
-const { fetchTransactions } = require('./lib/fetch')
+eventNames
+const { fetchTransactions } = require('./lib/fetch');
+const { eventNames } = require('./lib/googleClient');
 const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet } = require('./lib/update')
 
